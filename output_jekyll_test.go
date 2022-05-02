e38fce3097b3f0f9b4b4610dabc26319de842a05
Parker Moore <parkrmoore@gmail.com>
2022-05-02 15:15:22 -0700

    Move timestamp into less ambiguous territory (timezones)

--- output_jekyll_test.go
+++ output_jekyll_test.go
@@ -34,6 +34,7 @@ func TestJekyllOutputWriter_Preflight(t *testing.T) {
 	if err := w.Preflight(); err != nil {
 		t.Fatalf("preflight failed: %v", err)
 	}
+	defer cleanupTestTmpDir(jekyllOutputWriterTestDir)
 }
 
 func TestJekyllOutputWriter_Write(t *testing.T) {
@@ -50,7 +51,7 @@ func TestJekyllOutputWriter_Write(t *testing.T) {
 			Title:             "Title for the bookmark",
 			URL:               "https://example.com/bookmark1234",
 			ProgressTimestamp: 5678,
-			Time:              1288584076,
+			Time:              1288608076,
 			Progress:          0.5,
 			Starred:           "starred1234",
 		},
@@ -59,7 +60,7 @@ func TestJekyllOutputWriter_Write(t *testing.T) {
 			Title:     "Title for the bookmark",
 			Selection: "selection1234",
 			Folder:    instapaper.FolderIDUnread,
-			Timestamp: "1288584076",
+			Timestamp: "1288608076",
 			Hash:      "hash1234",
 		},
 		FullText: "full text\n\nof an article",
@@ -82,6 +83,6 @@ func TestJekyllOutputWriter_Write(t *testing.T) {
 	fileContentsMatch(t, filepath.Join(w.Directory, "_data", "1234.json"), `"Title": "Title for the bookmark",`)
 	fileContentsMatch(t, filepath.Join(w.Directory, "_data", "1234.highlights.json"), `"Text": "Text of the highlight",`)
 	fileContentsMatch(t, filepath.Join(w.Directory, "_mirror", "1234.html"), "full text\n\nof an article")
-	fileContentsMatch(t, filepath.Join(w.Directory, "_posts", "2010-10-31-1234.html"), `archive_id: "1234"`)
-	fileContentsMatch(t, filepath.Join(w.Directory, "_posts", "2010-10-31-1234.html"), "{% raw %}\nfull text\n\nof an article")
+	fileContentsMatch(t, filepath.Join(w.Directory, "_posts", "2010-11-01-1234.html"), `archive_id: "1234"`)
+	fileContentsMatch(t, filepath.Join(w.Directory, "_posts", "2010-11-01-1234.html"), "{% raw %}\nfull text\n\nof an article")
 }
